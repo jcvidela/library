eff6307496f0fb38ca709825e6d5b161157a7eae
jcvidela <juancarlos.videla00@gmail.com>
2019-10-10 19:02:37 -0300

    eliminando extension de desarrollo para deploy

--- src/store.js
+++ src/store.js
@@ -48,9 +48,9 @@ const initialState = {}
 
 //create store
 const store = createStoreWithFirebase(rootReducer, initialState, compose(
-	reactReduxFirebase(firebase),
-	window.__REDUX_DEVTOOLS_EXTENSION__ &&
-	window.__REDUX_DEVTOOLS_EXTENSION__()
+	reactReduxFirebase(firebase)
+	//window.__REDUX_DEVTOOLS_EXTENSION__ &&
+	//window.__REDUX_DEVTOOLS_EXTENSION__()
 ));
 
 export default store;
